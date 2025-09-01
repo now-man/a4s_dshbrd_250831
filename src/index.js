b202953e6f3d1d4d6b334c958431e4918476df4e
now-man <won3011506@gmail.com>
2025-09-01 09:56:28 +0000

    Feat: Merge two dashboard concepts and add new features

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './dashboard.css'; // 새로 추가된 CSS 파일
 import 'leaflet/dist/leaflet.css';
+import 'react-calendar/dist/Calendar.css'; // 캘린더 CSS
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

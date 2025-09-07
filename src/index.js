a7cb5a79e78cb2b2342056d44fb22e2589846fbb
now-man <won3011506@gmail.com>
2025-09-07 13:44:24 +0000

    Final Fix: Correcting all outstanding UI and logic bugs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import './dashboard.css';
-import 'react-calendar/dist/Calendar.css';
-
+import 'leaflet/dist/leaflet.css'; // 지도 UI 복원을 위해 필수적인 CSS import
+import 'react-day-picker/dist/style.css'; 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );

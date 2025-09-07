84ec4a9b21000bee3ad47f2f70d560165f984f80
now-man <won3011506@gmail.com>
2025-09-07 22:33:33 +0000

    Build: Revert to stable version and re-implement features

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'leaflet/dist/leaflet.css'; // 지도 UI 복원을 위해 필수적인 CSS import
+import 'leaflet/dist/leaflet.css';
 import 'react-day-picker/dist/style.css'; 
 import App from './App';
 
